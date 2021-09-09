f01785ac3f51dae22edf61719624fe40b706fc94
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-09 00:18:00 -0400

    update tables

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,2 +1,30 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS aabooks_db;
+CREATE DATABASE aabooks_db;
+
+
+USE aabooks_db;
+
+
+CREATE TABLE books (
+    id integer Primary key Auto_increment, 
+    author_lastname varchar(30),
+    author_firstname varchar(30),
+    title varchar(30)
+    );
+
+create table role (
+    id integer primary key Auto_increment  , 
+    title varchar(30), 
+    salary decimal(10,2),
+    department_id integer,
+    foreign key fk1(department_id) references department(id)
+);
+
+create table scholar (
+    id integer primary key,
+    first_name varchar(30), 
+    last_name varchar(30),
+    role_id integer,
+       mentor_id integer null,
+    foreign key fk2(role_id) references role(id)
+    );
--- Develop/db/scholarseeds.sql
+++ Develop/db/scholarseeds.sql
@@ -1,20 +1,20 @@
-use employees_db;
+use aabooks_db;
 
--- insert into employee (first_name,last_name,role_id,manager_id) values ('Jim','Spring',1,0);
--- insert into employee (first_name,last_name,role_id,manager_id) values ('Hallo','Cast',2,0);
--- insert into employee (first_name,last_name,role_id,manager_id) values ('Talli','Juckle',3,0);
--- insert into employee (first_name,last_name,role_id,manager_id) values ('Yoger','Max',4,0);
+-- insert into scholar (first_name,last_name,role_id,manager_id) values ('Jim','Spring',1,0);
+-- insert into scholar (first_name,last_name,role_id,manager_id) values ('Hallo','Cast',2,0);
+-- insert into scholar (first_name,last_name,role_id,manager_id) values ('Talli','Juckle',3,0);
+-- insert into scholar (first_name,last_name,role_id,manager_id) values ('Yoger','Max',4,0);
 
-insert into employee (first_name,last_name,role_id,manager_id) values ('Harper','Lee',5,1);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Jane','Meyers',6,2);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Denzel','Washington',7,3);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Chris','Tucker',8,4);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Harper','Lee',5,1);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Jane','Meyers',6,2);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Denzel','Washington',7,3);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Chris','Tucker',8,4);
 
-insert into employee (first_name,last_name,role_id,manager_id) values ('Opera','Goldberg',5,1);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Halle','Berry',6,2);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Don','Johnson',7,3);
-insert into employee (first_name,last_name,role_id,manager_id) values ('Eddie','King',8,4);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Opera','Goldberg',5,1);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Halle','Berry',6,2);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Don','Johnson',7,3);
+insert into scholar (first_name,last_name,role_id,manager_id) values ('Eddie','King',8,4);
 
-select * from employee;
+select * from scholar;
 
-delete from employee where id>4;
+delete from scholar where id>4;
