166a45ee10e12d71ad1deef1e2b829105519dadd
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-09 15:48:15 -0400

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -10,4 +10,21 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+/*Constructors Here?*/
+class Model {
+  constructor() {}
+}
+
+class View {
+  constructor() {}
+}
+
+class Controller {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+  }
+}
+
+const app = new Controller(new Model(), new View())
 module.exports = { User, Project };
