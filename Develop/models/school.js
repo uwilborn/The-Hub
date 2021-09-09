05bbb326a5357fb78053549e365be037c5ff6bb1
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-09 13:52:20 -0400

    Update school.js

--- Develop/models/school.js
+++ Develop/models/school.js
@@ -1,1 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class School extends Model {}
+
+School.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    schoolname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    schoollevel: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = School;
