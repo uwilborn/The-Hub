a23e47b68adc79835b0002ff10967d0c72633c35
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-09 00:13:50 -0400

    add books to database

---
+++ Develop/db/bookseeds.sql
@@ -0,0 +1,9 @@
+use employees_db;
+insert into department(name) values('R&D');
+insert into department(name) values('Software Engineering');
+insert into department(name) values('Media');
+insert into department(name) values('Production');
+
+select * from department;
+
+
---
+++ Develop/db/roleseeds.sql
@@ -0,0 +1,13 @@
+use employees_db;
+
+insert into role(title,salary,department_id)values('Manager',12093,1);
+insert into role(title,salary,department_id)values('Manager',3223,2);
+insert into role(title,salary,department_id)values('Manager',84344,3);
+insert into role(title,salary,department_id)values('Manager',23987,4);
+
+insert into role(title,salary,department_id)values('Team Lead',2093,1);
+insert into role(title,salary,department_id)values('Team Lead',223,2);
+insert into role(title,salary,department_id)values('Team Lead',8344,3);
+insert into role(title,salary,department_id)values('Team Lead',3987,4);
+
+select * from role;
