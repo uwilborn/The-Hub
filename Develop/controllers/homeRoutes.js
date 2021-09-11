f702b6bd3837c7172ace97f793818e610c6e2633
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-11 15:17:40 -0400

    Update login page

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -2,9 +2,17 @@ const router = require('express').Router()
 
 router.get("/",function(req,res){
     console.log("Home route")
-    res.render("landing")
+    res.render("landing____")
 })
 
+router.get("/profile",function(req,res){
+    console.log("profile")
+    res.render("profile")
+})
+router.get("/login",function(req,res){
+    console.log("login+++++")
+    res.render("login")
+})
 
 module.exports = router;
 
