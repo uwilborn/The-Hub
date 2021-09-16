36da5e1d6b99a6faa8d1a14edf6f9eb326f37033
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-16 11:04:39 -0400

    Create request.js

---
+++ Develop/public/js/request.js
@@ -0,0 +1,57 @@
+const requestFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const rfname = document.querySelector('#first-request').value.trim();
+  const rlname = document.querySelector('#last-request').value.trim();
+  const afname = document.querySelector('#author-name').value.trim();
+  const alname = document.querySelector('#password-login').value.trim();
+  const btitle = document.querySelector('#email-login').value.trim();
+  const bqty = document.querySelector('#quantity').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
