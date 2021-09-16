de9fb3c88f5f66438b180ff78b72ca9719f0f7dd
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-16 16:59:29 -0400

    Update request.js

--- Develop/public/js/request.js
+++ Develop/public/js/request.js
@@ -9,6 +9,84 @@ const requestFormHandler = async (event) => {
   const btitle = document.querySelector('#btitle').value.trim();
   const bqty = document.querySelector('#quantity').value.trim();
 
+  Option 1:
+  
+  <?php
+// Make a MySQL Connection
+mysql_connect("localhost", "username", "password") or die(mysql_error());
+mysql_select_db("aabooks") or die(mysql_error());
+
+// Insert a row of information into the table "user"
+mysql_query("INSERT INTO user 
+(first_name, last_name, author_firstname, author_lastname, book_title, quantity) VALUES(rfname, rlname, afname, alname, btitle, bqty) ") 
+or die(mysql_error());  
+
+echo "Data Inserted!";
+?>
+  
+  Option 2:
+  <?php
+include "dbConn.php"; // Using database connection file here
+
+if(isset($_POST['submit']))
+{		
+    $rfname = $_POST['first_name'];
+    $rlname = $_POST['last_name'];
+  $afname = $_POST['first_name'];
+    $alname = $_POST['age'];
+  $btitle= $_POST['first_name'];
+    $bqty = $_POST['age'];
+  
+
+    $insert = mysqli_query($db,"INSERT INTO `tblemp`(`fullname`, `age`) VALUES ('$fullname','$age')");
+
+    if(!$insert)
+    {
+        echo mysqli_error();
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
+}
+
+mysqli_close($db); // Close connection
+?>
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   if (rfname && rlname && afname && alname && btitle && bqty) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
